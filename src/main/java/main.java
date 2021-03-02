6024525e0f7d2c0ea19e9f30a5b4fb3d654d0a00
Jan Wienand <jan.wienand@microfocus.com>
2021-03-02 14:31:52 +0100

    Adding createApplication to create new app in ssc

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,7 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONObject;
 
 public class main {
 
@@ -29,9 +30,24 @@ public class main {
         System.out.println(licenseInformation.getBody().getObject().getJSONObject("data").getJSONArray("capabilities"));
     }
 
+    public static void createApplication() throws UnirestException {
+
+        String body = "{  \"name\": \"3\", \"description\": \"\", \"active\": true, \"committed\": false,  \"project\": {  \"name\": \"java\", \"description\": \"\", \"issueTemplateId\": \"Prioritized-HighRisk-Project-Template\"  }, \"issueTemplateId\": \"Prioritized-HighRisk-Project-Template\"}";
+
+        HttpResponse<JsonNode> createApplication = Unirest.post("http://18.197.33.109:8080/api/v1/projectVersions")
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .header("accept", "application/json")
+                .basicAuth("admin", "SSCpasswort123!")
+                .body(body)
+                .asJson();
+
+        System.out.println("Application created!");
+        System.out.println(createApplication.getBody());;
+    }
+
     public static void main(String[] args) throws UnirestException {
-        System.out.println("Hello");
-        testAPI();
+        //testAPI();
+        //createApplication();
 
     }
 }
