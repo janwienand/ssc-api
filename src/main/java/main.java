a0af5cc9030c5a46d77afd3059681d241368638b
Jan Wienand <jan.wienand@microfocus.com>
2021-03-03 17:37:54 +0100

    cleaning up

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -18,33 +18,10 @@ import java.util.concurrent.TimeUnit;
 public class main {
 
     private static String ssc = "http://35.156.197.254:8080";
-    private static String token = "ZDBhYzk4MDEtYmU4ZC00Nzg0LTk0M2EtZjE4YzE4ZWUzZmNm";
-    private static int projectVersion = 3;
+    public static String token;
+    public static String fileToken;
 
-    public static void testAPI() throws UnirestException {
-        HttpResponse<JsonNode> sscApiTest = Unirest.get(ssc + "/api/v1/tokens")
-                .header("Authorization", "FortifyToken " + token)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .header("accept", "application/json")
-                //ToDo
-                //encrypt password
-                .asJson();
-
-        System.out.println(sscApiTest.getBody().toString());
-    }
-    public static void getLicense() throws UnirestException {
-        HttpResponse<JsonNode> licenseInformation = Unirest.get(ssc + "/api/v1/license")
-                .header("Authorization", "FortifyToken " + token)
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .header("accept", "application/json")
-                //ToDo
-                //encrypt password
-                .asJson();
-
-        System.out.println(licenseInformation.getBody().getObject().getJSONObject("data").getJSONArray("capabilities"));
-    }
-
-    public static void createApplication() throws UnirestException {
+    public void createApplication() throws UnirestException {
 
         String body = "{  \"name\": \"1\",  \"description\": \"\",  \"active\": true,  \"committed\": false,  \"project\": {    \"name\": \"tokentest\",    \"description\": \"\",    \"issueTemplateId\": \"Prioritized-HighRisk-Project-Template\"  },  \"issueTemplateId\": \"Prioritized-HighRisk-Project-Template\"}";
 
@@ -71,7 +48,7 @@ public class main {
         System.out.println("Application created!");
         System.out.println(createApplication.getBody());
     }
-    public static void getProjectVersions() throws UnirestException {
+    public void getProjectVersions() throws UnirestException {
         HttpResponse<JsonNode> getListOfProjectVersions = Unirest.get(ssc + "/api/v1/projectVersions")
                 .header("Authorization", "FortifyToken " + token)
                 .header("Content-Type", "application/json; charset=UTF-8")
@@ -81,7 +58,9 @@ public class main {
         System.out.println(getListOfProjectVersions.getBody());
     }
 
-    public static void createToken() throws UnirestException {
+    //Use-Case 1
+    public void createToken(String tokenType) throws UnirestException {
+
         //create date with time for Token description
         LocalDateTime myDateObj = LocalDateTime.now();
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH:mm:ss");
@@ -90,7 +69,7 @@ public class main {
         //create body in JSON format
         JSONObject body = new JSONObject();
         body.put("description", formattedDate);
-        body.put("type", "UnifiedLoginToken");
+        body.put("type", tokenType);
 
         //get SSC credentials to create token
         auth sscAuth = new auth();
@@ -104,16 +83,19 @@ public class main {
                 .asJson();
 
         //store Token in variable token
-        String token = (String) createToken.getBody().getObject().getJSONObject("data").get("token");
-        System.out.println(token);
+        //String token = (String) createToken.getBody().getObject().getJSONObject("data").get("token");
+        token = (String) createToken.getBody().getObject().getJSONObject("data").get("token");
+
+        System.out.println(tokenType + " successfully created: " + token);
     }
 
-    public static void uploadResult() throws FileNotFoundException, UnirestException, InterruptedException {
+    //Use-Case 2
+    public void uploadResult(int projectVersion, String filePath) throws UnirestException, InterruptedException {
         //upload results
         HttpResponse<JsonNode> uploadResult = Unirest.post(ssc + "/api/v1/projectVersions/" + projectVersion + "/artifacts")
                 .header("Authorization", "FortifyToken " + token)
                 .header("accept", "application/json")
-                .field("file", new File("C:/dev/Benchmark-master/benchmark.fpr"))
+                .field("file", new File(filePath))
                 .asJson();
 
         //store response id to artifactId
@@ -135,13 +117,12 @@ public class main {
 
             if (currentStatus.equals("SCHED_PROCESSING") || currentStatus.equals("PROCESSING")){
                 System.out.println("current status: " + currentStatus + ". Wait another 5 seconds...");
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(5); //wait 5 seconds until next check
                 continue;
+
             } else if(currentStatus.equals("REQUIRE_AUTH")){
                 System.out.println("current status: " + currentStatus);
                 System.out.println("Waiting for approval");
-
-                TimeUnit.SECONDS.sleep(5);
 
                 //create list including artifactId
                 List<Integer> list = new ArrayList<>();
@@ -161,21 +142,37 @@ public class main {
                         .asJson();
 
                 System.out.println("Successfully approved");
-                i++;
+                i++; //end while loop
             } else {
                 System.out.println("current status: " + currentStatus);
-                i++;
+                i++; //end while loop
             }
         }
 
     }
+    //Download result test
+    public void downloadResult() throws UnirestException {
+        JSONObject body = new JSONObject();
+        body.put("fileTokenType", 1);
+
+        HttpResponse<JsonNode> getFileToken = Unirest.post(ssc + "/api/v1/fileTokens")
+                .header("Authorization", "FortifyToken " + token)
+                .header("Content-Type", "application/json")
+                .body(body)
+                .asJson();
+        fileToken = (String) getFileToken.getBody().getObject().getJSONObject("data").get("token");
+        System.out.println("File Token: " + fileToken);
+
+        HttpResponse<JsonNode> downloadResult = Unirest.get(ssc + "/download/artifactDownload.html?mat=" + fileToken + "&id=68")
+                .header("Authorization", "FortifyToken " + token)
+                .asJson();
+    }
+
 
     public static void main(String[] args) throws UnirestException, FileNotFoundException, InterruptedException {
-        //testAPI();
-        //createApplication();
-        //getProjectVersions();
-        //createToken();
-        uploadResult();
+        main test = new main();
+        test.createToken("UnifiedLoginToken");
+        //test.uploadResult(3, "C:/dev/Benchmark-master/benchmark.fpr");
 
     }
 }
