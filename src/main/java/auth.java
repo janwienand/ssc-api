0292e76a7e44c66a0fd961da2fec23ba23b301de
Jan Wienand <jan.wienand@microfocus.com>
2021-03-09 14:55:24 +0100

    cleaning up

---
+++ src/main/java/auth.java
@@ -0,0 +1,15 @@
+public class auth {
+
+    public String getUsername() {
+        String user = "<username>";
+        return user;
+    }
+    public String getPassword() {
+        String password = "<password>";
+        return password;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
